33268273486f614c245dbe6961ca29e45bbdea50
mtangaro <ma.tangaro@gmail.com>
2017-12-30 23:47:52 +0000

    add script

--- setup.py
+++ setup.py
@@ -2,23 +2,17 @@
 Galaxyctl
 '''
 
-# Always prefer setuptools over distutils
-from setuptools import setup, find_packages
-# To use a consistent encoding
-from codecs import open
-from os import path
+from setuptools import setup
 
-here = path.abspath(path.dirname(__file__))
-
-# Get the long description from the README file
-with open(path.join(here, 'README.md'), encoding='utf-8') as f:
-  long_description = f.read()
+def readme():
+    with open('README.rst') as f:
+        return f.read()
 
 setup(
   name='galaxyctl',
   version='0.1.0a1',
   description='galaxy,onedata and luks volume management',
-  long_description=long_description,
+  long_description=readme(),
   url='https://github.com/mtangaro/galaxyctl',
   author='Marco Antonio Tangaro, Federico Zambelli',
   author_email='ma.tangaro@ibiom.cnr.it', 
@@ -29,4 +23,5 @@ setup(
     'License :: OSI Approved :: MIT License',
   ],
   keywords='galaxy web server',
+  scripts=['bin/galaxyctl'],
 )
