e0b0519da0ca3482bc6f4a26051ea8204427533e
mtangaro <ma.tangaro@gmail.com>
2018-01-04 22:41:12 +0000

    make log level configurable

--- galaxyctl/common_logging.py
+++ galaxyctl/common_logging.py
@@ -6,9 +6,13 @@ import logging
 from .find_ini_file import find_ini_file
 from .read_ini_file import read_ini_file
 
-def set_log(log_level):
+def set_log():
 
-  galaxyctl_log_file = read_ini_file(find_ini_file('galaxyctl.ini'), 'galaxy', 'galaxyctl_log_file')
+  galaxyctl_config_file = find_ini_file('galaxyctl.ini')
+
+  galaxyctl_log_file = read_ini_file(galaxyctl_config_file, 'galaxy', 'galaxyctl_log_file')
+
+  log_level = read_ini_file(galaxyctl_config_file, 'galaxy', 'log_level')
 
   log = logging.getLogger(__name__)
 
