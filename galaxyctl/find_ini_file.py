4b1ef297792a41c26f30c5b12964e4d6b1c05682
mtangaro <ma.tangaro@gmail.com>
2018-01-04 11:24:51 +0000

    add function to correctly find config file

---
+++ galaxyctl/find_ini_file.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+''' Load INI Config File order(first found is used): ENV, /etc/ansible '''
+
+import sys
+
+def is_virtual():
+    """ Return if we run in a virtual environtment. """
+    # Check supports venv && virtualenv
+    return (getattr(sys, 'base_prefix', sys.prefix) != sys.prefix or
+            hasattr(sys, 'real_prefix'))
+
+def find_ini_file():
+
+  path='/etc/galaxyctl'
+  
+  if is_virtual() is True:
+    path = sys.prefix + '/etc/galaxyctl/'
+
+  return path 
