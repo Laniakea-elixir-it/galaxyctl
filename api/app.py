486db1cdf69515d90c127e66e072aabc74a45208
mtangaro <ma.tangaro@gmail.com>
2019-07-24 13:49:39 +0000

    add api

---
+++ api/app.py
@@ -0,0 +1,4 @@
+from galaxyctl_api import app
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0:5001', debug=True)
---
+++ api/galaxyctl_api.py
@@ -0,0 +1,32 @@
+from flask import Flask, abort, jsonify, request
+import requests
+
+import galaxyctl_run 
+
+app = Flask(__name__)
+
+@app.route('/galaxyctl_api/v1.0/galaxy-startup', methods=['POST'])
+def galaxy_startup():
+
+    # check if galaxy is online, if yes return online
+    # else run galaxy-startup script
+
+    if not request.json or not 'endpoint' in request.json:
+       print request.json
+       abort(400)
+
+    endpoint = request.json['endpoint']
+
+    response = requests.get(endpoint, verify=False)
+
+    sc = str(response.status_code)
+
+    if sc == '200' or sc == '302':
+      return jsonify({'galaxy': 'online' }) 
+
+    else:
+
+     return galaxyctl_run.galaxy_startup(endpoint)
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', debug=True)
